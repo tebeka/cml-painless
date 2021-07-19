96cf903e8d139ce10f612c71b84ee898f7e19438
Miki Tebeka <miki@353solutions.com>
2021-07-19 15:12:45 +0300

    painless

---
+++ painless.py
@@ -0,0 +1,58 @@
+"""Task used in auto scaler spot crash
+
+First time run, this task will spin a child process that will terminate the
+machine. Second time it'll succeed.
+"""
+
+from argparse import ArgumentParser
+from os import fork
+from subprocess import run
+from time import sleep
+
+from clearml import Task
+
+initial_state = 'initial'
+remote_first_state = 'remote_first'
+remote_second_state = 'remote_second'
+
+
+def terminate_instance():
+    cmd = ['sudo', 'shutdown', '-h', 'now']
+    print(' '.join(cmd))
+    return run(cmd).returncode == 0
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser(description='painless task, used in spot testing')
+    parser.add_argument('--queue', help='queue name', required=True)
+    args = parser.parse_args()
+
+    prop = 'painless_state'
+    task = Task.init(project_name='clearml-test', task_name='painless')
+    props = task.get_user_properties()
+
+    state = props.get(prop, initial_state)
+    task.log.info('painless: state=%r', state)
+
+    if state == initial_state:
+        task.set_user_properties(**{prop: remote_first_state})
+        task.execute_remotely(args.queue)
+
+    if state == remote_first_state:
+        task.set_user_properties(**{prop: remote_second_state})
+        if fork():
+            num_minutes = 10
+            task.log.info('painless: parent waiting for %s minutes', num_minutes)
+            for i in range(1, num_minutes * 60):
+                print('iteration {}'.format(i))
+                sleep(1)
+
+            task.log.info('painless: still here after %s minutes', num_minutes)
+            raise SystemExit('error: run full {} minutes'.format(num_minutes))
+
+        else:  # child process
+            task.log.info('painless: child terminating instance')
+            if not terminate_instance():
+                raise SystemExit('error: cannot terminate instance')
+
+    print('reborn!')
